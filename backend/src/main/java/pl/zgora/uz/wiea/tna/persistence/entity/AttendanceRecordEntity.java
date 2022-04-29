ccc9133669e099f704881eb7e4631c51a4d6a6b5
kubaWilk <jakub_wilk@outlook.com>
2022-04-29 19:38:38 +0200

    Created attendance classes for persistence

---
+++ backend/src/main/java/pl/zgora/uz/wiea/tna/persistence/entity/AttendanceRecordEntity.java
@@ -0,0 +1,59 @@
+package pl.zgora.uz.wiea.tna.persistence.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "attendance_records")
+public class AttendanceRecordEntity {
+
+    @Id
+    @SequenceGenerator(
+            name="attendance_records_id_seq",
+            sequenceName="attendance_records_id_seq",
+            allocationSize=1)
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "attendance_records_id_seq")
+    @Column(name = "id", unique = true, nullable = false)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @JoinColumn(name = "employee_id", referencedColumnName = "user_id")
+    private EmployeeEntity employeeEntity;
+
+    @ManyToOne(cascade = CascadeType.PERSIST, optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "shift_id", referencedColumnName = "id")
+    private ShiftEntity shiftEntity;
+
+    @Column(name = "entered_at", nullable = false)
+    private OffsetDateTime enteredAt;
+
+    @Column(name = "left_at")
+    private OffsetDateTime leftAt;
+
+    @Transient
+    private Long elapsedTimePerShiftInMinutes;
+}
