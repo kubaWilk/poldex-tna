5bbd18f890d20ea62b647f89450b96ecb783a454
monaboiste <monikaszumna4@gmail.com>
2022-05-09 20:58:11 +0200

    Added employee model

---
+++ backend/src/main/java/pl/zgora/uz/wiea/tna/persistence/entity/EmployeeEntity.java
@@ -0,0 +1,72 @@
+package pl.zgora.uz.wiea.tna.persistence.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+public class EmployeeEntity {
+
+    /**
+     * Shares PK with {@link UserEntity}
+     * See: {@link UserEntity#getId()}
+     */
+    @Id
+    @Column(name = "id", unique = true, nullable = false)
+    private Long id;
+
+    @Column(name = "firstname", nullable = false)
+    private String firstName;
+
+    @Column(name = "lastname", nullable = false)
+    private String lastName;
+
+    @Column(name = "department", nullable = false)
+    private String department;
+
+    @Column(name = "contract_id", unique = true, nullable = false)
+    private String contractId;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "post_code", columnDefinition = "zipcode")
+    private String postCode;
+
+    @Column(name = "city")
+    private String city;
+
+    @MapsId
+    @OneToOne(
+            cascade = CascadeType.REMOVE,
+            fetch = FetchType.LAZY,
+            optional = false,
+            orphanRemoval = true)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @PrimaryKeyJoinColumn(name = "id", referencedColumnName = "id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private UserEntity userEntity;
+
+    // TODO: add attendance records
+}
---
+++ backend/src/main/java/pl/zgora/uz/wiea/tna/service/EmployeeService.java
@@ -0,0 +1,91 @@
+package pl.zgora.uz.wiea.tna.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.zgora.uz.wiea.tna.persistence.entity.EmployeeEntity;
+import pl.zgora.uz.wiea.tna.persistence.entity.UserEntity;
+import pl.zgora.uz.wiea.tna.persistence.repository.EmployeeRepository;
+import pl.zgora.uz.wiea.tna.service.exception.ContractIdViolationException;
+import pl.zgora.uz.wiea.tna.service.exception.UserNotFoundException;
+import pl.zgora.uz.wiea.tna.util.StringUtils;
+
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+    private final UserService userService;
+
+    public List<EmployeeEntity> fetchAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public EmployeeEntity fetchEmployeeById(long employeeId) {
+        return employeeRepository.findById(employeeId)
+                .orElseThrow(UserNotFoundException::new);
+    }
+
+    @Transactional
+    public EmployeeEntity createEmployee(EmployeeEntity employeeEntity) {
+        validateContractId(employeeEntity.getContractId());
+        prettifyFields(employeeEntity);
+
+        final String username = generateDefaultUsername(employeeEntity);
+        final String password = generateDefaultPassword();
+
+        final UserEntity userEntity = UserEntity.builder()
+                .username(username)
+                .password(password)
+                .build();
+        final UserEntity savedUserEntity = userService.createUser(userEntity);
+        employeeEntity.setUserEntity(savedUserEntity);
+
+        return employeeRepository.saveAndFlush(employeeEntity);
+    }
+
+    public boolean employeeExistsById(long employeeId) {
+        return employeeRepository.existsById(employeeId);
+    }
+
+    @Transactional
+    public void deleteEmployee(long employeeId) {
+        if (!employeeRepository.existsById(employeeId)) {
+            throw new UserNotFoundException();
+        }
+        employeeRepository.deleteById(employeeId);
+    }
+
+    private String generateDefaultUsername(final EmployeeEntity employeeEntity) {
+        final char firstLetterOfName = employeeEntity.getFirstName().toLowerCase().charAt(0);
+        final String lastname = employeeEntity.getLastName().split(" ")[0].toLowerCase();
+        final String lastThreeContractNumbers = employeeEntity.getContractId()
+                .substring(employeeEntity.getContractId().length() - 3);
+
+        return firstLetterOfName + lastname + lastThreeContractNumbers;
+    }
+
+    private String generateDefaultPassword() {
+        return "test";
+    }
+
+    private void prettifyFields(final EmployeeEntity employeeEntity) {
+        employeeEntity.setFirstName(StringUtils.toTitleCase(employeeEntity.getFirstName()));
+        employeeEntity.setLastName(StringUtils.toTitleCase(employeeEntity.getLastName()));
+        employeeEntity.setDepartment(StringUtils.toTitleCase(employeeEntity.getDepartment()));
+        employeeEntity.setStreet(StringUtils.toTitleCase(employeeEntity.getStreet()));
+        employeeEntity.setCity(StringUtils.toTitleCase(employeeEntity.getCity()));
+        employeeEntity.setContractId(employeeEntity.getContractId().toUpperCase());
+    }
+
+    private void validateContractId(final String contractId) {
+        if (Objects.isNull(contractId)
+                || contractId.length() < 3
+                || employeeRepository.existsByContractId(contractId.toUpperCase())) {
+            throw new ContractIdViolationException();
+        }
+    }
+}
