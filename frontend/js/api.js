670272da8af6220eff5f23fd4ea8d3d3da35978f
wsapon <sapon.wojciech@outlook.com>
2022-05-10 19:35:14 +0200

    merged from LoginForm branch

---
+++ frontend/js/api.js
@@ -0,0 +1,19 @@
+// const BASE_URL = 'https://tna-system-backend.herokuapp.com';
+const BASE_URL = 'http://localhost:8080';
+const PREFIX = '/api';
+const API_URL = `${BASE_URL}${PREFIX}`;
+
+/* export default class Api {
+    static async fetchCurrentUser() {
+        const requestOptions = {
+            method: 'GET',
+            headers: {
+                'Content-type': 'application/json',
+                'Authorization': `Basic + ${localStorage.getItem('token')}`,
+            },
+        };
+        return fetch(`${API_URL}/users/currenst`, requestOptions);
+    }
+}
+ */
+export { API_URL as apiUrl };
---
+++ frontend/js/login.js
@@ -0,0 +1,53 @@
+import { apiUrl } from './api.js';
+
+$('#loginButton').click(e => {
+    e.preventDefault();
+    // see: login.html
+    localStorage.clear();
+
+    const username = $('#username').val();
+    const password = $('#password').val();
+    const b64token = btoa(`${username}:${password}`);
+    let headers = new Headers();
+    headers.append('Content-type', 'application/json')
+    headers.append('Authorization', 'Basic ' + b64token);
+
+    let requestOptions = {
+        method: 'GET',
+        headers: headers
+    };
+
+    fetch(`${apiUrl}/users/current`, requestOptions)
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+            return Promise.reject(response);
+        })
+        .then(user => {
+            if (user) {
+                localStorage.setItem('token', b64token);
+                localStorage.setItem('userId', user.id);
+                localStorage.setItem('userRole', user.role);
+                localStorage.setItem('loggedIn', true);
+
+                return fetch(`${apiUrl}/employees/${user.id}`, requestOptions);
+            }
+        })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            return Promise.reject(response);
+        })
+        .then(employee => {
+            localStorage.setItem('employeeFirstname', employee.firstName);
+            localStorage.setItem('employeeLastname', employee.lastName);
+
+            document.location.replace('index.html');
+        })
+        .catch(err => {
+            console.error(err);
+            $('#loginError').text('Niepoprawne dane logowania, spróbuj ponownie.');
+        });
+});
