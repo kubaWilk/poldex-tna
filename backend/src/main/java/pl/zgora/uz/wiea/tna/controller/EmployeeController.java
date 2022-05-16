6616e8e3db3971333871ad9b3cd2a14f7fd3f06e
kubaWilk <jakub_wilk@outlook.com>
2022-05-16 19:10:57 +0200

    Changed employeeControler and filled in sample-data.sql

--- backend/src/main/java/pl/zgora/uz/wiea/tna/controller/EmployeeController.java
+++ backend/src/main/java/pl/zgora/uz/wiea/tna/controller/EmployeeController.java
@@ -5,15 +5,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import pl.zgora.uz.wiea.tna.model.AttendanceRecord;
+import pl.zgora.uz.wiea.tna.model.AttendanceRecordEntryTime;
+import pl.zgora.uz.wiea.tna.model.AttendanceRecordExitTime;
 import pl.zgora.uz.wiea.tna.model.Employee;
+import pl.zgora.uz.wiea.tna.persistence.entity.AttendanceRecordEntity;
 import pl.zgora.uz.wiea.tna.persistence.entity.EmployeeEntity;
+import pl.zgora.uz.wiea.tna.service.AttendanceRecordService;
 import pl.zgora.uz.wiea.tna.service.EmployeeService;
+import pl.zgora.uz.wiea.tna.util.AttendanceRecordUtils;
 import pl.zgora.uz.wiea.tna.util.EmployeeUtils;
 
 import javax.validation.Valid;
@@ -26,6 +33,7 @@ import java.util.stream.Collectors;
 public class EmployeeController {
 
     private final EmployeeService employeeService;
+    private final AttendanceRecordService attendanceRecordService;
 
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
@@ -49,10 +57,58 @@ public class EmployeeController {
         return EmployeeUtils.mapEmployeeEntityToEmployee(employeeEntity);
     }
 
+    @GetMapping("/{employeeId}/attendance")
+    public List<AttendanceRecord> fetchAllEmployeesAttendanceRecords(@PathVariable long employeeId) {
+        final List<AttendanceRecordEntity> attendanceRecordEntities
+                = attendanceRecordService.fetchAllAttendanceRecordsByEmployeeId(employeeId);
+        return attendanceRecordEntities.stream()
+                .map(AttendanceRecordUtils::mapAttendanceRecordEntityToAttendanceRecord)
+                .collect(Collectors.toList());
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PatchMapping("/{employeeId}/attendance/{attendanceId}/entry-time")
+    public AttendanceRecord updateEntryTime(
+            @PathVariable long employeeId,
+            @PathVariable("attendanceId") long attendanceId,
+            @RequestBody @Valid final AttendanceRecordEntryTime attendanceRecordEntryTime) {
+        return AttendanceRecordUtils.mapAttendanceRecordEntityToAttendanceRecord(
+                attendanceRecordService.updateEntryTime(
+                        employeeId,
+                        attendanceId,
+                        attendanceRecordEntryTime.getEnteredAt()
+                ));
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PatchMapping("/{employeeId}/attendance/{attendanceId}/exit-time")
+    public AttendanceRecord updateExitTime(
+            @PathVariable long employeeId,
+            @PathVariable("attendanceId") long attendanceId,
+            @RequestBody @Valid final AttendanceRecordExitTime attendanceRecordExitTime) {
+        return AttendanceRecordUtils.mapAttendanceRecordEntityToAttendanceRecord(
+                attendanceRecordService.updateExitTime(
+                        employeeId,
+                        attendanceId,
+                        attendanceRecordExitTime.getLeftAt()
+                ));
+    }
+
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{employeeId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteEmployee(@PathVariable("employeeId") long employeeId) {
         employeeService.deleteEmployee(employeeId);
+    }
+
+    @PatchMapping("/{employeeId}/attendance/exit-time")
+    public AttendanceRecord updateExitTimeOfRecentAttendance(
+            @PathVariable long employeeId,
+            @RequestBody @Valid final AttendanceRecordExitTime attendanceRecordExitTime) {
+        AttendanceRecordEntity attendanceRecordEntity
+                = attendanceRecordService.updateExitTimeOfRecentAttendance(
+                employeeId, attendanceRecordExitTime);
+        return AttendanceRecordUtils
+                .mapAttendanceRecordEntityToAttendanceRecord(attendanceRecordEntity);
     }
 }
